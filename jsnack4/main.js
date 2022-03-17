64551cbb24c891112f940354a0d5369b18f81699
dibss <ludovicad01@gmail.com>
2022-03-17 17:40:34 +0100

    .

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -1,5 +1,3 @@
-// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. Interrompi il ciclo appena il nome è stato trovato.
-
 const nomi = [
   "Robert",
   "Jennifer",
